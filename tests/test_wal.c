d4608895efa9b7251ca3bb18be50ad6159b5fb38
Narezzzs <nareswara353@gmail.com>
2026-09-11 21:24:42 +0700

    test(wal): replace assert with CHECK macro to survive NDEBUG builds

--- tests/test_wal.c
+++ tests/test_wal.c
@@ -1,12 +1,19 @@
 #include "lsm_tree.h"
 #include "lsm_types.h"
 
-#include <assert.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#define CHECK(cond)                                                                                \
+    do {                                                                                           \
+        if (!(cond)) {                                                                             \
+            fprintf(stderr, "CHECK failed at %s:%d: %s\n", __FILE__, __LINE__, #cond);            \
+            exit(1);                                                                               \
+        }                                                                                          \
+    } while (0)
 
 typedef struct Wal Wal;
 typedef void (*WalRecoverCallback)(uint8_t op, LsmSlice key, LsmSlice value, SequenceNumber seq,
@@ -52,35 +59,35 @@ static void test_wal_roundtrip(void) {
     unlink(path);
 
     Wal *wal = NULL;
-    assert(wal_open(path, false, &wal) == LSM_OK);
-    assert(wal != NULL);
+    CHECK(wal_open(path, false, &wal) == LSM_OK);
+    CHECK(wal != NULL);
 
-    assert(wal_append_put(wal, lsm_slice_str("alpha"), lsm_slice_str("one"), 1) == LSM_OK);
-    assert(wal_append_put(wal, lsm_slice_str("beta"), lsm_slice_str("two"), 2) == LSM_OK);
-    assert(wal_append_delete(wal, lsm_slice_str("alpha"), 3) == LSM_OK);
-    assert(wal_sync(wal) == LSM_OK);
+    CHECK(wal_append_put(wal, lsm_slice_str("alpha"), lsm_slice_str("one"), 1) == LSM_OK);
+    CHECK(wal_append_put(wal, lsm_slice_str("beta"), lsm_slice_str("two"), 2) == LSM_OK);
+    CHECK(wal_append_delete(wal, lsm_slice_str("alpha"), 3) == LSM_OK);
+    CHECK(wal_sync(wal) == LSM_OK);
     wal_close(wal);
 
-    assert(wal_open(path, false, &wal) == LSM_OK);
+    CHECK(wal_open(path, false, &wal) == LSM_OK);
     ReplayLog log;
     memset(&log, 0, sizeof(log));
-    assert(wal_recover(wal, capture, &log) == LSM_OK);
+    CHECK(wal_recover(wal, capture, &log) == LSM_OK);
     wal_close(wal);
 
-    assert(log.count == 3);
-    assert(log.ops[0] == 1);
-    assert(strcmp(log.keys[0], "alpha") == 0);
-    assert(strcmp(log.values[0], "one") == 0);
-    assert(log.seqs[0] == 1);
+    CHECK(log.count == 3);
+    CHECK(log.ops[0] == 1);
+    CHECK(strcmp(log.keys[0], "alpha") == 0);
+    CHECK(strcmp(log.values[0], "one") == 0);
+    CHECK(log.seqs[0] == 1);
 
-    assert(log.ops[1] == 1);
-    assert(strcmp(log.keys[1], "beta") == 0);
-    assert(strcmp(log.values[1], "two") == 0);
-    assert(log.seqs[1] == 2);
+    CHECK(log.ops[1] == 1);
+    CHECK(strcmp(log.keys[1], "beta") == 0);
+    CHECK(strcmp(log.values[1], "two") == 0);
+    CHECK(log.seqs[1] == 2);
 
-    assert(log.ops[2] == 2);
-    assert(strcmp(log.keys[2], "alpha") == 0);
-    assert(log.seqs[2] == 3);
+    CHECK(log.ops[2] == 2);
+    CHECK(strcmp(log.keys[2], "alpha") == 0);
+    CHECK(log.seqs[2] == 3);
 
     unlink(path);
 }
@@ -90,14 +97,14 @@ static void test_wal_sync_flag(void) {
     unlink(path);
 
     Wal *wal = NULL;
-    assert(wal_open(path, true, &wal) == LSM_OK);
-    assert(wal_append_put(wal, lsm_slice_str("k"), lsm_slice_str("v"), 42) == LSM_OK);
+    CHECK(wal_open(path, true, &wal) == LSM_OK);
+    CHECK(wal_append_put(wal, lsm_slice_str("k"), lsm_slice_str("v"), 42) == LSM_OK);
     wal_close(wal);
 
     int fd = open(path, O_RDONLY);
-    assert(fd >= 0);
+    CHECK(fd >= 0);
     off_t size = lseek(fd, 0, SEEK_END);
-    assert(size > 0);
+    CHECK(size > 0);
     close(fd);
 
     unlink(path);
@@ -108,23 +115,23 @@ static void test_wal_empty_recover(void) {
     unlink(path);
 
     Wal *wal = NULL;
-    assert(wal_open(path, false, &wal) == LSM_OK);
+    CHECK(wal_open(path, false, &wal) == LSM_OK);
     wal_close(wal);
 
-    assert(wal_open(path, false, &wal) == LSM_OK);
+    CHECK(wal_open(path, false, &wal) == LSM_OK);
     ReplayLog log;
     memset(&log, 0, sizeof(log));
-    assert(wal_recover(wal, capture, &log) == LSM_OK);
-    assert(log.count == 0);
+    CHECK(wal_recover(wal, capture, &log) == LSM_OK);
+    CHECK(log.count == 0);
     wal_close(wal);
 
     unlink(path);
 }
 
 static void test_wal_null_args(void) {
-    assert(wal_open(NULL, false, NULL) == LSM_ERR_INVALID_ARG);
-    assert(wal_sync(NULL) == LSM_ERR_INVALID_ARG);
-    assert(wal_recover(NULL, NULL, NULL) == LSM_ERR_INVALID_ARG);
+    CHECK(wal_open(NULL, false, NULL) == LSM_ERR_INVALID_ARG);
+    CHECK(wal_sync(NULL) == LSM_ERR_INVALID_ARG);
+    CHECK(wal_recover(NULL, NULL, NULL) == LSM_ERR_INVALID_ARG);
 }
 
 int main(void) {
